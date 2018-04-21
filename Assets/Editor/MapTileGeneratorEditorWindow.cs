b9f48b3248b38b83ed39d33560f5e640b0c2d476
epolekoff <epolekoff@gmail.com>
2018-04-20 20:01:14 -0700

    Created an editor script to generate maps.

---
+++ Assets/Editor/MapTileGeneratorEditorWindow.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MapTileGeneratorEditorWindow : EditorWindow
+{
+    public int Width;
+    public int Height;
+    public GameObject Parent;
+    public GameObject MapTilePrefab;
+
+    // Add menu item to the Window menu
+    [MenuItem("Window/Map Tile Generator")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        MapTileGeneratorEditorWindow window = (MapTileGeneratorEditorWindow)EditorWindow.GetWindow(typeof(MapTileGeneratorEditorWindow));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+
+        Width = EditorGUILayout.IntField("Width", Width);
+        Height = EditorGUILayout.IntField("Height", Height);
+        Parent = (GameObject)EditorGUILayout.ObjectField("Parent", Parent, typeof(GameObject), allowSceneObjects: true);
+        MapTilePrefab = (GameObject)EditorGUILayout.ObjectField("Map Tile", MapTilePrefab, typeof(GameObject), allowSceneObjects: false);
+
+        // Generate all of the map tiles.
+        if (GUILayout.Button("Generate"))
+        {
+            for(int x = 0; x < Width; x++)
+            {
+                for(int y = 0; y < Height; y++)
+                {
+                    GameObject.Instantiate(
+                        MapTilePrefab, 
+                        new Vector3(
+                            x * MapTile.Width, 
+                            0,
+                            y * MapTile.Width), 
+                        Quaternion.identity, Parent.transform);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Map/MapTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTile : MonoBehaviour
+{
+    // All tiles are squares, and the same size.
+    public static float Width = 1f;
+
+    // Tiles have modifiable height.
+    // They default to 1, but you can change it.
+    public float Height = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
