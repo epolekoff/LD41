288699fc37303d8ee739bf06a485d5f41114f3ce
epolekoff <epolekoff@gmail.com>
2018-04-21 01:33:53 -0700

    Health bar setup

--- Assets/Scripts/Shooter/Shooter.cs
+++ Assets/Scripts/Shooter/Shooter.cs
@@ -7,18 +7,26 @@ public class Shooter : MonoBehaviour {
     public ShooterData Data;
     public GameObject Visual;
     public Transform CameraSocket;
+    public ShooterCanvas ShooterCanvas;
 
     public Vector2 TilePosition;
 
     public int TeamId { get; set; }
 
+    private int m_currentHealth;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        // Set starting values.
+        m_currentHealth = Data.Health;
+        ShooterCanvas.SetHealth(1);
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
@@ -31,5 +39,30 @@ public class Shooter : MonoBehaviour {
         {
             renderer.material = teamColor;
         }
+    }
+
+    /// <summary>
+    /// Take some amount of damage.
+    /// </summary>
+    public void TakeDamage(int damage)
+    {
+        m_currentHealth -= damage;
+        if(m_currentHealth <= 0)
+        {
+            Die();
+        }
+
+        ShooterCanvas.SetHealth(m_currentHealth / Data.Health);
+    }
+
+    /// <summary>
+    /// When this player dies, do some stuff.
+    /// </summary>
+    private void Die()
+    {
+        // Destroy the game object.
+        //GameObject.Destroy(this.gameObject);
+
+        // Remove the object from the map tile system.
     }
 }
---
+++ Assets/Scripts/Shooter/ShooterCanvas.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShooterCanvas : MonoBehaviour
+{
+
+    public Image HealthbarFill;
+    public Image HealthbarBackground;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Show or hide the health bars.
+    /// </summary>
+    /// <param name="show"></param>
+    public void ShowHealthBar(bool show)
+    {
+        HealthbarBackground.gameObject.SetActive(show);
+        HealthbarFill.gameObject.SetActive(show);
+    }
+
+    /// <summary>
+    /// Set the health of this shooter.
+    /// </summary>
+    public void SetHealth(float ratio)
+    {
+        // If Health is at full, hide the health bar.
+        if(ratio == 1)
+        {
+            ShowHealthBar(false);
+        }
+
+        HealthbarFill.fillAmount = ratio;
+    }
+}
